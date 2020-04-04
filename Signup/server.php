1438c6c00254005ccf9edd6742e68dd9599bc113
Sivew <33907602+Sivew@users.noreply.github.com>
2020-04-04 23:57:59 +0530

    Update server.php

--- Signup/server.php
+++ Signup/server.php
@@ -2,7 +2,10 @@
 	session_start();
 
 	// variable declaration
-	$username = "";
+	$firstname = "";
+	$lastname = "";
+	$indosno = "";
+	$dob = "";
 	$email    = "";
 	$errors = array(); 
 	$_SESSION['success'] = "";
@@ -13,13 +16,19 @@
 	// REGISTER USER
 	if (isset($_POST['reg_user'])) {
 		// receive all input values from the form
-		$username = mysqli_real_escape_string($db, $_POST['username']);
+		$firstname = mysqli_real_escape_string($db, $_POST['firstname']);
+		$lastname = mysqli_real_escape_string($db, $_POST['lastname']);
+		$indosno = mysqli_real_escape_string($db, $_POST['indosno']);
+		$dob = mysqli_real_escape_string($db, $_POST['dob']);
 		$email = mysqli_real_escape_string($db, $_POST['email']);
 		$password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
 		$password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
 
 		// form validation: ensure that the form is correctly filled
-		if (empty($username)) { array_push($errors, "Username is required"); }
+		if (empty($firstname)) { array_push($errors, "Firstname is required"); }
+		if (empty($lastname)) { array_push($errors, "Lastname is required"); }
+		if (empty($indosno)) { array_push($errors, "INDoSNo is required"); }
+		if (empty($dob)) { array_push($errors, "DoB is required"); }
 		if (empty($email)) { array_push($errors, "Email is required"); }
 		if (empty($password_1)) { array_push($errors, "Password is required"); }
 
@@ -30,10 +39,10 @@
 		// register user if there are no errors in the form
 		if (count($errors) == 0) {
 			$password = md5($password_1);//encrypt the password before saving in the database
-			$query = "INSERT INTO Sign_up_entry (username, email, password) 
-					  VALUES('$username', '$email', '$password')";
-			mysqli_query($db, $query);
-
+			$query = "INSERT INTO Sign_up_entry (firstname, lastname, indosno, dob, email, password) 
+					  VALUES('$firstname', '$lastname', '$indosno', '$dob', '$email', '$password')";
+			mysqli_query($db, $query)or die(mysqli_error($db));
+			$username = $firstname ." ". $lastname;
 			$_SESSION['username'] = $username;
 			$_SESSION['success'] = "You are now logged in";
 			header('location: index.php');
@@ -45,11 +54,11 @@
 
 	// LOGIN USER
 	if (isset($_POST['login_user'])) {
-		$username = mysqli_real_escape_string($db, $_POST['username']);
+		$email = mysqli_real_escape_string($db, $_POST['email']);
 		$password = mysqli_real_escape_string($db, $_POST['password']);
 
-		if (empty($username)) {
-			array_push($errors, "Username is required");
+		if (empty($email)) {
+			array_push($errors, "Email is required");
 		}
 		if (empty($password)) {
 			array_push($errors, "Password is required");
@@ -57,8 +66,13 @@
 
 		if (count($errors) == 0) {
 			$password = md5($password);
-			$query = "SELECT * FROM Sign_up_entry WHERE username='$username' AND password='$password'";
+			$query = "SELECT * FROM Sign_up_entry WHERE email='$email' AND password='$password'";
 			$results = mysqli_query($db, $query);
+			 while ($row = $results->fetch_assoc()) {
+				$field1name = $row["firstname"];
+				$field2name = $row["lastname"];
+			 }	
+			$username = $field1name ." ". $field2name;
 
 			if (mysqli_num_rows($results) == 1) {
 				$_SESSION['username'] = $username;
